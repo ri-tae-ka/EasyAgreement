fa2e67b77368355f05d34914789a937f0ff8fdbc
ri-tae-ka <ritzthareja@gmail.com>
2023-04-30 22:44:53 +0530

    accessibility menu read and increase font

--- client/src/component/accessibility/menu.js
+++ client/src/component/accessibility/menu.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const menu = () => {
-  return <div>menu</div>;
-};
+function Menu({
+  fontSize,
+  onFontSizeChange,
+  readAloud,
+  onReadAloudChange,
+  description,
+  summary,
+}) {
+  const [increaseCount, setIncreaseCount] = useState(0);
 
-export default menu;
+  const handleIncreaseFontSize = () => {
+    setIncreaseCount((prevCount) => prevCount + 1);
+
+    if (increaseCount === 5) {
+      onFontSizeChange(16);
+      setIncreaseCount(0);
+    } else {
+      onFontSizeChange((prevSize) => prevSize + 2);
+    }
+  };
+
+  const handleToggleReadAloud = () => {
+    if (readAloud) {
+      speechSynthesis.cancel();
+    }
+    onReadAloudChange(!readAloud);
+  };
+
+  useEffect(() => {
+    if (readAloud) {
+      const utterance = new SpeechSynthesisUtterance(
+        "Description: " + description + " Summary: " + summary
+      );
+      utterance.onend = () => {
+        onReadAloudChange(false);
+      };
+      speechSynthesis.speak(utterance);
+    }
+  }, [readAloud, description, summary]);
+
+  return (
+    <div>
+      <button className="font-size-button" onClick={handleIncreaseFontSize}>
+        Increase Font Size
+      </button>
+      <button className="read-aloud-button" onClick={handleToggleReadAloud}>
+        {readAloud ? "Stop Reading" : "Read Aloud"}
+      </button>
+      <button>Translate <div id="translate"></div></button>
+    </div>
+  );
+}
+
+export default Menu;
--- client/src/component/summary/SingleSummary.js
+++ client/src/component/summary/SingleSummary.js
@@ -1,8 +1,9 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import "./SingleSummary.css";
 import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { getSingleSummary } from "../../actions/summaryActions";
+import Menu from "../accessibility/menu";
 
 const SingleSummary = () => {
   const dispatch = useDispatch();
@@ -14,10 +15,13 @@ const SingleSummary = () => {
 
   const { isAuthenticated } = useSelector((state) => state.user);
 
+  const [fontSize, setFontSize] = useState(16);
+  const [readAloud, setReadAloud] = useState(false);
+
   useEffect(() => {
     dispatch(getSingleSummary(id));
 
-    if (!isAuthenticated) {
+    if (isAuthenticated === "false") {
       window.location = "/";
     }
   }, [dispatch, id]);
@@ -31,6 +35,14 @@ const SingleSummary = () => {
 
   return (
     <Fragment>
+      <Menu
+        fontSize={fontSize}
+        readAloud={readAloud}
+        onFontSizeChange={setFontSize}
+        onReadAloudChange={setReadAloud}
+        description={summary.description}
+        summary={summary.summary}
+      />
       <div className="single-summary-page">
         {loading ? (
           <p>Loading...</p>
@@ -40,10 +52,12 @@ const SingleSummary = () => {
               <h3 className="summary-name">{summary.name}</h3>
               <p className="summary-date">{formattedDate}</p>
             </div>
-            <div className="summary-body">
+            <h4>Description</h4>
+            <div className="summary-body" style={{ fontSize }}>
               <p>{summary.description}</p>
             </div>
-            <div className="summary-body">
+            <h4>Summary</h4>
+            <div className="summary-body" style={{ fontSize }}>
               <p>{summary.summary}</p>
             </div>
           </div>
