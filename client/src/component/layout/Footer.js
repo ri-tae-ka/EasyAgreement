7e33b18ce9515cf6671c9c8c335c09b7c30a6f60
ri-tae-ka <ritzthareja@gmail.com>
2023-05-06 15:44:40 +0530

    footer component

--- client/src/component/layout/Footer.js
+++ client/src/component/layout/Footer.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { Fragment } from "react";
+import "./Footer.css";
+import logo from "../../images/logo.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Fragment>
+      <footer>
+        <div class="container">
+          <div className="top-footer">
+            <div className="left-top-footer">
+              <img src={logo} />
+              <p>Easy Agreement</p>
+            </div>
+            <div className="left-top-footer">
+              <h3>Contact Us</h3>
+              <p>easyagreement@gmail.com</p>
+            </div>
+          </div>
+          <div className="bottom-footer">
+            <p>&copy; 2023 Easy Agreement. All rights reserved.</p>
+          </div>
+        </div>
+      </footer>
+    </Fragment>
+  );
 };
 
 export default Footer;
